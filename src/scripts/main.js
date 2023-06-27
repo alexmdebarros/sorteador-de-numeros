8654c9f7eba82360487dc688b356afccab3e1ab4
Alex Martins <alexmdebarros@gmail.com>
2023-06-27 16:31:35 -0400

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento){
+        evento.preventDefault();
+
+        let numMaximo = document.getElementById('numero-maximo').value;
+        numMaximo = parseInt(numMaximo);
+
+        let numAleatorio = Math.random() * numMaximo;
+        numAleatorio = Math.floor(numAleatorio + 1);
+        
+        document.getElementById('resultado-sorteio').innerText = numAleatorio;
+        document.querySelector('.resultado').style.display = 'block';
+    })
+})
